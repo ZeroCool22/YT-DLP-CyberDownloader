c886273e9fdae496f66d8d9070bd652533c55ce1
ZeroCool22 <ZeroCool22@users.noreply.github.com>
2026-07-14 23:08:53 -0300

    Remove local development launcher

--- lanzar.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Lanzar CyberDownloader
-:: Ejecuta la app usando pythonw.exe del venv para no abrir ventanas de consola negras feas de fondo
-start "" "%~dp0venv\Scripts\pythonw.exe" "%~dp0main.py"
-exit
